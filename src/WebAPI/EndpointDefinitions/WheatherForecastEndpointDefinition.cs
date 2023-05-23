4310203af2d968d141166b6b27cd4c6b14b853c2
Bernardo Salgueiro <bsalgueiro@gmail.com>
2023-05-23 18:23:13 +0100

    Added WebAPI Project
    Added WebAPI Project with Minimal API

---
+++ src/WebAPI/EndpointDefinitions/WheatherForecastEndpointDefinition.cs
@@ -0,0 +1,19 @@
+﻿using Carter;
+
+namespace WebAPI.EndpointDefinitions;
+
+public class WheatherForecastEndpointDefinition : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/wheatherforecast", GetWheatherForecast)
+            .WithName("GetWeatherForecast")
+            .WithOpenApi();
+    }
+
+    internal IResult GetWheatherForecast(HttpContext context)
+    {
+        // TO DO: Get data from Weather Forecast Query
+        return Results.Ok();
+    }
+}
